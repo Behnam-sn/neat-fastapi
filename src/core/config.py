42eb38f5b9e345be0789a9c8cde750d483b3b967
behnam <salehinezhad.b@gmail.com>
2021-10-18 17:18:00 +0330

    add core

---
+++ src/core/config.py
@@ -0,0 +1,28 @@
+import secrets
+from typing import List, Union
+from pydantic import AnyHttpUrl, BaseSettings, validator
+
+
+class Settings(BaseSettings):
+    API_V1_STR: str = "/api/v1"
+    SECRET_KEY: str = secrets.token_urlsafe(32)
+    # 60 minutes * 24 hours * 8 days = 8 days
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = ["http://localhost:8080"]
+
+    @validator("BACKEND_CORS_ORIGINS", pre=True)
+    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
+        if isinstance(v, str) and not v.startswith("["):
+            return [i.strip() for i in v.split(",")]
+        elif isinstance(v, (list, str)):
+            return v
+        raise ValueError(v)
+
+    PROJECT_NAME: str = "neat"
+    SQLALCHEMY_DATABASE_URL: str = "sqlite:///./sql_app.db"
+
+    class Config:
+        case_sensitive = True
+
+
+settings = Settings()
