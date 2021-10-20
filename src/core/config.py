925323800b986ec2e92af4b976a862a5ff0376cd
behnam <salehinezhad.b@gmail.com>
2021-10-20 10:55:11 +0330

    minor change

--- src/core/config.py
+++ src/core/config.py
@@ -12,7 +12,9 @@ class Settings(BaseSettings):
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = [
-        "http://localhost:8080", "https://localhost:8080"]
+        "http://localhost:8080",
+        "https://localhost:8080"
+    ]
 
     @validator("BACKEND_CORS_ORIGINS", pre=True)
     def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
