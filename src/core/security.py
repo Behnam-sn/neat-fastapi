0ad89229ca18c7eb0ea8695ba29e457418d234cb
behnam <salehinezhad.b@gmail.com>
2021-10-20 23:37:09 +0330

    update security token

--- src/core/security.py
+++ src/core/security.py
@@ -1,4 +1,4 @@
-from typing import Any, Union
+from typing import Optional
 from datetime import datetime, timedelta
 
 from jose import jwt
@@ -12,7 +12,17 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 ALGORITHM = "HS256"
 
 
-def create_access_token(subject: Union[str, Any], expires_delta: timedelta = None) -> str:
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
+    to_encode = data.copy()
+
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
 
@@ -21,17 +31,9 @@ def create_access_token(subject: Union[str, Any], expires_delta: timedelta = Non
             minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
         )
 
-    to_encode = {"exp": expire, "sub": str(subject)}
+    to_encode.update({"exp": expire})
     return jwt.encode(
         to_encode,
         settings.SECRET_KEY,
         algorithm=ALGORITHM
     )
-
-
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
-
-
-def get_password_hash(password: str) -> str:
-    return pwd_context.hash(password)
