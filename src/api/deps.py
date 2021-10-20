fb4ed5abebceead32deecad76c80bde057ac8b68
behnam <salehinezhad.b@gmail.com>
2021-10-20 23:32:13 +0330

    add api deps

---
+++ src/api/deps.py
@@ -0,0 +1,52 @@
+from typing import Generator
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from sqlalchemy.orm import Session
+
+from src import crud, models, schemas
+from src.core import security
+from src.core.config import settings
+from src.database.session import SessionLocal
+
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl=f"{settings.API_V1_STR}/login/access-token"
+)
+
+
+def get_db() -> Generator:
+    try:
+        db = SessionLocal()
+        yield db
+
+    finally:
+        db.close()
+
+
+def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)) -> models.User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[security.ALGORITHM]
+        )
+
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+
+        token_data = schemas.TokenData(username=username)
+
+    except JWTError:
+        raise credentials_exception
+
+    user = crud.user.get(db, username=token_data.username)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return user
--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -1,3 +1,3 @@
-from .token import Token, TokenPayload
+from .token import Token, TokenData
 from .note import Note, NoteCreate, NoteInDB, NoteUpdate
 from .user import User, UserCreate, UserInDB, UserUpdate
