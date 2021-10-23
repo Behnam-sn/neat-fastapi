04c40822a5a23ff8f869a78388dfceadc07179a1
behnam <salehinezhad.b@gmail.com>
2021-10-23 22:41:39 +0330

    update api notes

--- src/api/api_v1/endpoints/notes.py
+++ src/api/api_v1/endpoints/notes.py
@@ -9,85 +9,108 @@ from src.api import deps
 router = APIRouter()
 
 
+@router.get("/public-all", response_model=List[schemas.Note])
+def get_all_public_notes(
+    skip: int = 0,
+    limit: int = 100,
+    db: Session = Depends(deps.get_db)
+):
+    return crud.get_public_notes(db, skip=skip, limit=limit)
+
+
+@router.get("/public-author", response_model=List[schemas.Note])
+def get_public_notes_by_author(
+    author: str,
+    db: Session = Depends(deps.get_db),
+):
+    db_user = crud.get_user_by_username(db, username=author)
+
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return crud.get_public_notes_by_author(db, author=author)
+
+
+@router.get("/public-id", response_model=schemas.Note)
+def get_public_note_by_id(
+    id: int,
+    db: Session = Depends(deps.get_db),
+):
+    db_note = crud.get_note_by_id(db, id=id)
+
+    if db_note is None:
+        raise HTTPException(status_code=404, detail="Note not found")
+
+    if db_note.public == 0:
+        raise HTTPException(status_code=400, detail="Note is not public")
+
+    return db_note
+
+
 @router.post("/", response_model=schemas.Note)
 def create_note(
     note: schemas.NoteCreate,
     current_user: models.User = Depends(deps.get_current_user),
     db: Session = Depends(deps.get_db),
 ):
-
     return crud.create_note(db, note=note, author=current_user.username)
 
 
-@router.get("/public", response_model=List[schemas.Note])
-def get_public_notes(
-    skip: int = 0,
-    limit: int = 100,
-    db: Session = Depends(deps.get_db)
-):
-
-    return crud.get_public_notes(db, skip=skip, limit=limit)
-
-
 @router.get("/", response_model=List[schemas.Note])
-def get_user_notes(
+def get_current_user_notes(
     current_user: Optional[models.User] = Depends(deps.get_current_user),
     db: Session = Depends(deps.get_db),
 ):
-
-    return crud.get_currnet_user_notes(db, author=current_user.username)
-
-
-@router.get("/author", response_model=List[schemas.Note])
-def get_notes_by_author(
-    author: str,
-    db: Session = Depends(deps.get_db),
-):
-
-    return crud.get_notes_by_author(db, author=author)
+    return crud.get_notes_by_author(db, author=current_user.username)
 
 
 @router.get("/id", response_model=schemas.Note)
 def get_note_by_id(
     id: int,
+    current_user: Optional[models.User] = Depends(deps.get_current_user),
     db: Session = Depends(deps.get_db),
 ):
+    db_note = crud.get_note_by_id(db, id=id)
 
-    return crud.get_note_by_id(db, id=id)
+    if db_note is None:
+        raise HTTPException(status_code=404, detail="Note not found")
+
+    if db_note.author != current_user.username:
+        raise HTTPException(status_code=400, detail="Permission denied")
+
+    return db_note
 
 
 @router.put("/", response_model=schemas.Note)
 def update_note(
-    note: schemas.Note,
+    id: int,
     note_update: schemas.NoteUpdate,
     current_user: models.User = Depends(deps.get_current_user),
     db: Session = Depends(deps.get_db),
 ):
-
-    if current_user.username != note.author:
-        raise HTTPException(status_code=400, detail="Permission denied")
-
-    db_note = crud.get_note_by_id(db, id=note.id)
+    db_note = crud.get_note_by_id(db, id=id)
 
     if db_note is None:
         raise HTTPException(status_code=404, detail="Note not found")
 
-    return crud.update_note(db, id=note.id, note_update=note_update)
+    if db_note.author != current_user.username:
+        raise HTTPException(status_code=400, detail="Permission denied")
+
+    return crud.update_note(db, id=id, note_update=note_update)
 
 
 @router.delete("/", response_model=schemas.Note)
 def delete_note(
-    note: schemas.Note,
+    id: int,
     current_user: models.User = Depends(deps.get_current_user),
     db: Session = Depends(deps.get_db),
 ):
-
-    if current_user.username != note.author:
-        raise HTTPException(status_code=400, detail="Permission denied")
-
-    db_note = crud.get_note_by_id(db, id=note.id)
+    db_note = crud.get_note_by_id(db, id=id)
 
     if db_note is None:
         raise HTTPException(status_code=404, detail="Note not found")
 
-    return crud.remove_note(db, id=note.id)
+    if db_note.author != current_user.username:
+        raise HTTPException(status_code=400, detail="Permission denied")
+
+    return crud.remove_note(db, id=id)
--- src/crud/note.py
+++ src/crud/note.py
@@ -19,34 +19,6 @@ def create_note(db: Session, note: schemas.NoteCreate, author: str) -> models.No
     return db_note
 
 
-def get_note_by_id(db: Session, id: int) -> Optional[models.Note]:
-    return (
-        db.query(models.Note)
-        .filter(models.Note.id == id, models.Note.public == 1)
-        .first()
-    )
-
-
-def get_currnet_user_notes(db: Session, author: str, skip: int = 0, limit: int = 100) -> List[models.Note]:
-    return (
-        db.query(models.Note)
-        .filter(models.Note.author == author)
-        .offset(skip)
-        .limit(limit)
-        .all()
-    )
-
-
-def get_notes_by_author(db: Session, author: str, skip: int = 0, limit: int = 100) -> List[models.Note]:
-    return (
-        db.query(models.Note)
-        .filter(models.Note.author == author, models.Note.public == 1)
-        .offset(skip)
-        .limit(limit)
-        .all()
-    )
-
-
 def get_public_notes(db: Session, skip: int = 0, limit: int = 100) -> List[models.Note]:
     return (
         db.query(models.Note)
@@ -57,8 +29,36 @@ def get_public_notes(db: Session, skip: int = 0, limit: int = 100) -> List[model
     )
 
 
+def get_public_notes_by_author(db: Session, author: str, skip: int = 0, limit: int = 100) -> List[models.Note]:
+    return (
+        db.query(models.Note)
+        .filter(models.Note.author == author, models.Note.public == 1)
+        .offset(skip)
+        .limit(limit)
+        .all()
+    )
+
+
+def get_notes_by_author(db: Session, author: str, skip: int = 0, limit: int = 100) -> List[models.Note]:
+    return (
+        db.query(models.Note)
+        .filter(models.Note.author == author)
+        .offset(skip)
+        .limit(limit)
+        .all()
+    )
+
+
+def get_note_by_id(db: Session, id: int) -> Optional[models.Note]:
+    return (
+        db.query(models.Note)
+        .filter(models.Note.id == id)
+        .first()
+    )
+
+
 def update_note(db: Session, id: int, note_update: schemas.NoteUpdate) -> models.Note:
-    db_note = db.query(models.Note).filter(models.Note.id == id).first()
+    db_note = get_note_by_id(db, id=id)
 
     update_data = note_update.dict(exclude_unset=True)
     update_data["modified_at"] = datetime.datetime.now()
@@ -72,7 +72,7 @@ def update_note(db: Session, id: int, note_update: schemas.NoteUpdate) -> models
 
 
 def remove_note(db: Session, id: int) -> models.Note:
-    db_note = db.query(models.Note).filter(models.Note.id == id).first()
+    db_note = get_note_by_id(db, id=id)
     db.delete(db_note)
     db.commit()
     return db_note
