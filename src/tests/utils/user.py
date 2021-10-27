09faf26fde1dd53265bc7106b6cf91166b9621ad
behnam <salehinezhad.b@gmail.com>
2021-10-27 18:02:50 +0330

    update test utils

--- src/tests/utils/user.py
+++ src/tests/utils/user.py
@@ -1,28 +1,25 @@
-from sqlalchemy.orm import Session
-from fastapi.testclient import TestClient
+from src.tests.conftest import client
 
-from src import crud
-from src.models.user import User
-from src.schemas.user import UserCreate
 from src.core.config import settings
-from src.tests.utils.utils import username, password
 
 
-def create_the_random_user(db: Session) -> User:
-    user_in = UserCreate(username=username, password=password)
-    return crud.create_user(db, user=user_in)
+def create_random_user_by_api(username: str, password: str):
+    data = {"username": username, "password": password}
+
+    client.post(
+        f"{settings.API_V1_STR}/login/signup",
+        json=data
+    )
 
 
-def delete_the_random_user(db: Session) -> User:
-    return crud.remove_user(db, username=username)
-
-
-def user_authentication_headers(client: TestClient, u, p):
-    data = {"username": u, "password": p}
+def user_authentication_headers(username: str, password: str):
+    data = {"username": username, "password": password}
 
     response = client.post(
-        f"{settings.API_V1_STR}/login/token", data=data
+        f"{settings.API_V1_STR}/login/token",
+        data=data
     )
     tokens = response.json()
     auth_token = tokens["access_token"]
+
     return {"Authorization": f"Bearer {auth_token}"}
