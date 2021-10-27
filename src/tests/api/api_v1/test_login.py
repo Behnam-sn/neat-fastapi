fca352d6732c3ea7b807fd988f4d3d431ff6087a
behnam <salehinezhad.b@gmail.com>
2021-10-27 12:54:30 +0330

    update test api

--- src/tests/api/api_v1/test_login.py
+++ src/tests/api/api_v1/test_login.py
@@ -1,25 +1,27 @@
-from fastapi.testclient import TestClient
+from src.tests.conftest import client
 
 from src.core.config import settings
-from src.tests.utils.utils import username, password
+from src.tests.utils.utils import random_lower_string
+from src.tests.utils.user import create_random_user_by_api
 
 
-def test_create_user(client: TestClient):
-    data = {"username": username, "password": password}
+def test_login():
+    username = random_lower_string()
+    password = random_lower_string()
+
+    create_random_user_by_api(username=username, password=password)
+
+    data = {
+        "username": username,
+        "password": password
+    }
 
     response = client.post(
-        f"{settings.API_V1_STR}/login/signup", json=data
-    )
-    assert response.status_code == 200
-
-
-def test_login(client: TestClient):
-    data = {"username": username, "password": password}
-
-    response = client.post(
-        f"{settings.API_V1_STR}/login/token", data=data
+        f"{settings.API_V1_STR}/login/token",
+        data=data
     )
     tokens = response.json()
+
     assert response.status_code == 200
     assert "access_token" in tokens
     assert tokens["access_token"]
--- src/tests/api/api_v1/test_users.py
+++ src/tests/api/api_v1/test_users.py
@@ -1,65 +1,105 @@
-from fastapi.testclient import TestClient
+from src.tests.conftest import client
 
 from src.core.config import settings
-from src.tests.utils.user import user_authentication_headers
-from src.tests.utils.utils import random_lower_string, username, password, new_username, new_password
+from src.tests.utils.utils import random_lower_string
+from src.tests.utils.user import create_random_user_by_api, user_authentication_headers
 
 
-def test_create_existing_user(client: TestClient):
-    data = {"username": username, "password": password}
+def test_create_user():
+    username = random_lower_string()
+    password = random_lower_string()
+
+    data = {
+        "username": username,
+        "password": password
+    }
 
     response = client.post(
-        f"{settings.API_V1_STR}/login/signup", json=data
+        f"{settings.API_V1_STR}/login/signup",
+        json=data
     )
+
+    assert response.status_code == 200
+
+
+def test_create_existing_user():
+    username = random_lower_string()
+    password = random_lower_string()
+
+    create_random_user_by_api(username=username, password=password)
+
+    data = {
+        "username": username,
+        "password": password
+    }
+
+    response = client.post(
+        f"{settings.API_V1_STR}/login/signup",
+        json=data
+    )
+
     assert response.status_code == 400
 
 
-def test_get_all_users(client: TestClient):
-    response = client.get(
-        f"{settings.API_V1_STR}/users/all"
-    )
-    assert response.status_code == 200
+# def test_get_all_users(client: TestClient):
+#     response = client.get(
+#         f"{settings.API_V1_STR}/users/all"
+#     )
+#     assert response.status_code == 200
 
 
-def test_get_user(client: TestClient):
+def test_get_user():
+    username = random_lower_string()
+    password = random_lower_string()
+
+    create_random_user_by_api(username=username, password=password)
+    token = user_authentication_headers(username=username, password=password)
+
     response = client.get(
         f"{settings.API_V1_STR}/users/",
-        headers=user_authentication_headers(
-            client=client, u=username, p=password
-        )
+        headers=token,
     )
     user = response.json()
-    assert response.status_code == 200
+
     assert user["username"] == username
 
 
-def test_update_user(client: TestClient):
+def test_update_user():
+    username = random_lower_string()
+    password = random_lower_string()
+
+    create_random_user_by_api(username=username, password=password)
+    token = user_authentication_headers(username=username, password=password)
+
     data = {
-        "username": new_username,
-        "password": new_password,
+        "username": random_lower_string(),
+        "password": random_lower_string(),
         "full_name": random_lower_string()
     }
 
     response = client.put(
         f"{settings.API_V1_STR}/users/",
-        headers=user_authentication_headers(
-            client=client, u=username, p=password
-        ),
+        headers=token,
         json=data
     )
     user = response.json()
-    assert response.status_code == 200
-    assert user["username"] == new_username
+
+    assert user["username"] == data["username"]
     assert user["full_name"] == data["full_name"]
 
 
-def test_delete_user(client: TestClient):
+def test_delete_user():
+    username = random_lower_string()
+    password = random_lower_string()
+
+    create_random_user_by_api(username=username, password=password)
+    token = user_authentication_headers(username=username, password=password)
+
     response = client.delete(
         f"{settings.API_V1_STR}/users/",
-        headers=user_authentication_headers(
-            client=client, u=new_username, p=new_password
-        ),
+        headers=token,
     )
     user = response.json()
+
     assert response.status_code == 200
-    assert user["username"] == new_username
+    assert user["username"] == username
