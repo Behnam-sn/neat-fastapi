8339bb6dde784e10ee96bc17e98be07352bc79a1
behnam <salehinezhad.b@gmail.com>
2021-10-27 18:01:46 +0330

    update test crud user

--- src/tests/crud/test_user.py
+++ src/tests/crud/test_user.py
@@ -3,22 +3,32 @@ from sqlalchemy.orm import Session
 from src import crud
 from src.core.security import verify_password
 from src.schemas.user import UserCreate, UserUpdate
-from src.tests.utils.utils import random_lower_string, username, new_username, password, new_password
+from src.tests.utils.user import create_random_user_by_api
+from src.tests.utils.utils import random_lower_string
 
 
 def test_create_user(db: Session):
+    username = random_lower_string()
+    password = random_lower_string()
+
     user_in = UserCreate(username=username, password=password)
     user_obj = crud.create_user(db, user=user_in)
+
     assert user_obj.username == username
     assert hasattr(user_obj, "hashed_password")
 
 
 def test_authenticate_user(db: Session):
+    username = random_lower_string()
+    password = random_lower_string()
+
+    create_random_user_by_api(username=username, password=password)
     authenticated_user = crud.authenticate_user(
         db,
         username=username,
         password=password
     )
+
     assert authenticated_user
     assert authenticated_user.username == username
 
@@ -29,21 +39,36 @@ def test_not_authenticate_user(db: Session):
         username=random_lower_string(),
         password=random_lower_string()
     )
+
     assert user is None
 
 
 def test_get_all_users(db: Session):
     users = crud. get_users(db)
+
     assert users
 
 
 def test_get_user(db: Session):
+    username = random_lower_string()
+    password = random_lower_string()
+
+    create_random_user_by_api(username=username, password=password)
     user = crud.get_user_by_username(db, username=username)
+
     assert user
     assert user.username == username
 
 
 def test_update_user(db: Session):
+    username = random_lower_string()
+    password = random_lower_string()
+
+    create_random_user_by_api(username=username, password=password)
+
+    new_username = random_lower_string()
+    new_password = random_lower_string()
+
     user_in_update = UserUpdate(
         username=new_username,
         password=new_password,
@@ -51,12 +76,18 @@ def test_update_user(db: Session):
     )
     crud.update_user(db, username=username, user_update=user_in_update)
     user = crud.get_user_by_username(db, username=new_username)
+
     assert user
     assert user.full_name
     assert verify_password(new_password, user.hashed_password)
 
 
 def test_delete_user(db: Session):
-    user = crud.remove_user(db, username=new_username)
+    username = random_lower_string()
+    password = random_lower_string()
+
+    create_random_user_by_api(username=username, password=password)
+    user = crud.remove_user(db, username=username)
+
     assert user
-    assert user.username == new_username
+    assert user.username == username
