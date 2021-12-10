8f17e294931f60dd06f6b288c9c4c6ab25aefad5
behnam <salehinezhad.b@gmail.com>
2021-12-10 04:39:09 +0330

    update cors

--- src/core/config.py
+++ src/core/config.py
@@ -16,7 +16,7 @@ class Settings(BaseSettings):
         "https://localhost:3000",
         "http://localhost:5000",
         "https://localhost:5000",
-        "https://condescending-bartik-1ef502.netlify.app"
+        "https://behnamneat.netlify.app/"
     ]
 
     @validator("BACKEND_CORS_ORIGINS", pre=True)
