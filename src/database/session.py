3c7c81127b7f723c66db04e7d255e2e89c15846f
behnam <salehinezhad.b@gmail.com>
2022-07-23 01:48:26 +0430

    add postgres

--- src/database/session.py
+++ src/database/session.py
@@ -1,13 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
 from src.core.config import settings
 
+uri = settings.DATABASE_URL
+if uri and uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 
-engine = create_engine(
-    settings.SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(uri)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
